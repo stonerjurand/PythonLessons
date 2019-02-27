e4250a20d891b66c89fed5581fcafed81647164d
Yurand <stonerjurand@gmail.com>
2019-02-27 17:57:20 +0300

    easy and normal partially done

--- lesson-5/home_work/hw05_easy.py
+++ lesson-5/home_work/hw05_easy.py
@@ -2,9 +2,43 @@
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+import os
+import sys
+#from hw05_normal import dir_name
+
+def make_dir(dir_name):
+    if not dir_name:
+        print("Необходимо указать имя директории вторым параметром")
+        return
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.mkdir(dir_path)
+        print(f'директория {dir_name} создана')
+    except FileExistsError:
+        print(f'директория {dir_name} уже существует')
+
+if __name__ == "__main__":
+    for i in range(1,10):
+        dir_name = f'dir_{i}'
+        make_dir()
+
+
+
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-
+def list_dir():
+    files = os.listdir(path=os.getcwd())
+    print(files)
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+def make_copy():
+    cur_path = sys.argv[0].split('/')
+    file_name = cur_path[-1]
+    copy_name = file_name.replace('.', '_copy.')
+    os.popen(f'copy {file_name} {copy_name}')
+
+# try:
+#     dir_name = sys.argv[1]
+# except IndexError:
+#     dir_name = None
--- lesson-5/home_work/hw05_normal.py
+++ lesson-5/home_work/hw05_normal.py
@@ -13,3 +13,34 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+import sys
+import hw05_easy as e
+
+
+while True:
+    print('Текущая директория = ', os.getcwd())
+    key = input(
+        "Введите команду из списка:\n"
+        "1. Перейти в папку - chdir <имя папки>\n"
+        "2. Просмотреть содержимое текущей папки - ls\n"
+        "3. Удалить папку - del <имя папки>\n"
+        "4. Создать папку - mkdir <имя папки>\n"
+        "5. Выйти из программы - exit\n"
+    )
+    key = key.split(' ')
+    if key[0] == 'exit':
+        sys.exit()
+
+    if key[0] == 'mkdir':
+        try:
+            #global dir_name
+            dir_name = key[1]
+            e.make_dir(dir_name)
+        except IndexError:
+            print("Необходимо указать имя директории вторым параметром")
+
+
+# key = 'mkdir newDir'
+# print(key.split(' '))
